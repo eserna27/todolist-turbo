6ac7987cc5a1116c4144291a726f6b7fedeb3105
eserna27 <eserna27@gmail.com>
2022-01-13 18:03:31 -0600

    can edit list title

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'todolists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :new, :show, :create, :destroy]
+  resources :lists, only: [:index, :new, :show, :create, :destroy, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
   root "lists#index"
